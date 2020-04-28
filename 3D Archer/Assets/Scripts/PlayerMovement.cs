48ddf1b39b34d12af40f3417e91c367cdea3d866
Daniel Olkowski <olkowski.daniel@gmail.com>
2020-04-28 23:37:21 +0200

    Player walk animation in unity

---
+++ 3D Archer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    CharacterController characterController;
+    Animator animator;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        
+    }
+  
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (moveDirection != Vector3.zero)
+            animator.SetBool("isWalking", true);
+        else
+            animator.SetBool("isWalking", false);
+    }
+}
